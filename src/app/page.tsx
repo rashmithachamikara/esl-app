9353b02b366b4aaa3274e5b07f24bfea20bbdf54
rashmithachamikara <rashmitha.chamickara@gmail.com>
2025-02-27 00:42:24 +0530

    Modified home page

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function RootPage() {
+  return (
+    <div
+      className="bg-zinc-900"
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1 className="text-6xl font-bold" style={{ color: "#D93639" }}>Shelf Link</h1>
+    </div>
+  );
+}
