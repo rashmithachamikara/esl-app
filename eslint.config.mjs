600c850a57dd6f664988edc1bc5b5f8cdfd5c5bb
Amri <muhammedamri2002@gmail.com>
2025-03-01 02:38:15 +0530

    Saved All files so the prettier and ESlint rules will be applied in every file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript", "prettier"],
     rules: {
-      semi: ["error"],
+      //semi: ["error"],
       quotes: ["error", "double"],
     },
   }),
--- src/app/demo/typography-demo/page.tsx
+++ src/app/demo/typography-demo/page.tsx
@@ -1,20 +1,22 @@
 export default function TypographyDemo() {
+  const demoStyle: React.CSSProperties = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "20px",
+    padding: "64px",
+  };
 
-    const demoStyle: React.CSSProperties = {
-        display: "flex",
-        flexDirection: "column",
-        gap: "20px",
-        padding: "64px"
-    }
-
-    return (
+  return (
     <div style={demoStyle}>
       <h1>Hello There H1</h1>
       <h2>Hello There H2</h2>
       <h3>Hello There H3</h3>
       <h4>Hello There H4</h4>
       <p>Hello There Paragraph</p>
-      <p>Let's migrate to another approach later on. For now, use html tags. Styles are defined in css/typography.css and imported to root layout.tsx</p>
+      <p>
+        Let's migrate to another approach later on. For now, use html tags.
+        Styles are defined in css/typography.css and imported to root layout.tsx
+      </p>
     </div>
   );
 }
