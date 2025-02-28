377cafafb46a473900bce41f16cc2af4876cd5d8
rashmithachamikara <rashmitha.chamickara@gmail.com>
2025-02-28 15:44:44 +0530

    Formatted code to match prettier standards

--- src/components/design-card.tsx
+++ src/components/design-card.tsx
@@ -7,7 +7,7 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Pencil, Trash2 } from "lucide-react";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function DesignCard({
   title,
@@ -17,21 +17,35 @@ export default function DesignCard({
   imageUrl: string;
 }) {
   return (
-    <Card className="mr-16 h-65"> {/* Custom card size */}
+    <Card className="mr-16 h-65">
+      {" "}
+      {/* Custom card size */}
       <CardHeader className="mb-0 pb-1">
-        <CardTitle className="text-sm">{title}</CardTitle> {/* Reduce font size */}
+        <CardTitle className="text-sm">{title}</CardTitle>{" "}
+        {/* Reduce font size */}
       </CardHeader>
       <CardContent>
         <div>
-          <Image src={imageUrl} className="resized-image" alt={title} layout="responsive" width={500} height={300} /> {/* Adjust image size */}
+          <Image
+            src={imageUrl}
+            className="resized-image"
+            alt={title}
+            layout="responsive"
+            width={500}
+            height={300}
+          />{" "}
+          {/* Adjust image size */}
         </div>
       </CardContent>
-      <CardFooter className="flex justify-between mt-0 mb-0"> {/* Add padding to ensure buttons are inside */}
-      <Button>
+      <CardFooter className="flex justify-between mt-0 mb-0">
+        {" "}
+        {/* Add padding to ensure buttons are inside */}
+        <Button>
           <Pencil /> Edit
         </Button>
         <Button className="bg-red-900 text-white">
-          <Trash2/>Delete
+          <Trash2 />
+          Delete
         </Button>
       </CardFooter>
     </Card>
