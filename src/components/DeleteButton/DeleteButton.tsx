949efd370ed93e53b3a2f7d9dab36cf7a64c3f46
rashmithachamikara <rashmitha.chamickara@gmail.com>
2025-03-02 05:59:40 +0530

    Updated delete button with correct colors and hover effects

--- src/components/DeleteButton/DeleteButton.tsx
+++ src/components/DeleteButton/DeleteButton.tsx
@@ -9,12 +9,16 @@ interface DeleteButtonProps {
   className?: string;
 }
 
-export default function DeleteButton({ label, onClick, className }: DeleteButtonProps) {
+export default function DeleteButton({
+  label,
+  onClick,
+  className,
+}: DeleteButtonProps) {
   return (
     <Button
       onClick={onClick}
-      className={`bg-[#7F1D1D] text-white text-lg font-medium rounded-md px-6 py-3 flex items-center gap-2 
-        hover:bg-[#7F1D1D] active:bg-[#7F1D1D] focus:bg-[#7F1D1D]${className}`}
+      className={`bg-red-900 text-white text-lg font-medium rounded-md px-6 py-3 flex items-center gap-2 
+        hover:bg-red-800 active:bg-[#7F1D1D] focus:bg-[#7F1D1D]${className}`}
     >
       <Trash2 className="w-6 h-6" /> {label}
     </Button>
